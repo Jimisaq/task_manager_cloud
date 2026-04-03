baa362f08f5936c3cea2374880f1b2f11ac4aaeb
Jim_isaq <jimisaac8082@gmail.com>
2026-04-03 19:52:34 +0300

    Added CORS config

--- src/main/java/org/jimisaac/task_manager_cloud_assignment/config/WebConfig.java
+++ src/main/java/org/jimisaac/task_manager_cloud_assignment/config/WebConfig.java
@@ -11,8 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .exposedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
